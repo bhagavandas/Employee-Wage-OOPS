b8a32546e6e7b5e6a3d94d67c238efbfe2605542
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-07-30 08:17:53 +0530

    Added: UC2

--- src/employeeWage/WelcomeEmpWage.java
+++ src/employeeWage/WelcomeEmpWage.java
@@ -1,11 +1,12 @@
 package employeeWage;
 
-public class WelcomeEmpWage extends UC1{
+public class WelcomeEmpWage extends UC2{
 	
 
 	public static void main(String[] args) {
 		
-		AttendanceCheck();
+		//AttendanceCheck();
+		DailyEmpWage();
 	}
 
 
